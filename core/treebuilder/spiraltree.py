af0e6333fb684364d4c2c71ed5d3800994b8bda8
glebpinigin <pinigin1514@live.com>
2022-02-27 11:41:27 +0300

    moving spiraltree to nerworkx

--- core/treebuilder/spiraltree.py
+++ core/treebuilder/spiraltree.py
@@ -1,29 +1,21 @@
-class SpiralTreeNode():
-    
-    def __init__(self, R=None, parent=None, root_xy=None):
-        self.R = R
-        self.parent = parent
-        self.xy = R.leaf if root_xy is None else root_xy
-        self.children = []
-    
-    def get_polar(self):
-        return {"dst": self.R.dst, # angle and dst from NodeRegion
-                "ang": self.R.ang}
-    
+import networkx as nx
 
+class SpiralTree(nx.DiGraph):
+    
+    def __init__(self):
+        super().__init__()
+        self.add_node("root")
+    
+    def insertLeaf(self, R):
+        self.add_node(R)
+        self.add_edge("root", R)
+    
+    def insertSteinerNode(self, *args, steiner_R=None, leaf1_R=None, leaf2_R=None):
+        for i, param in enumerate(steiner_R, leaf1_R, leaf2_R):
+            param = args[i] if param is None else param
 
-class SpiralTree():
-    
-    def __init__(self, root_xy):
-        self.root = SpiralTreeNode(root_xy = root_xy)
-    
-    def insertLeaf(self, R) -> SpiralTreeNode:
-        leaf_node = SpiralTreeNode(R)
-        self.root.children.append(leaf_node)
-        return leaf_node
-    
-    def insertSteinerNode(self, steiner_node, leaf_node1: SpiralTreeNode, leaf_node2: SpiralTreeNode):
-        self.root.children.append(steiner_node)
-        steiner_node.children = [leaf_node1, leaf_node2]
-        self.root.children.remove(leaf_node1)
-        self.root.children.remove(leaf_node2)
+        self.add_node(steiner_R)
+        self.add_edge("root", steiner_R)
+        for leaf_R in (leaf1_R, leaf2_R):
+          self.remove_edge("root", leaf_R)
+          self.add_edge(steiner_R, leaf_R)
