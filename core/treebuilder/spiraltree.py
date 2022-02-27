c26b8c4be6c6cfb1b326a27a1ce90e67adbbc792
glebpinigin <pinigin1514@live.com>
2022-02-27 11:59:19 +0300

    insertSteinerNode edited

--- core/treebuilder/spiraltree.py
+++ core/treebuilder/spiraltree.py
@@ -10,10 +10,7 @@ class SpiralTree(nx.DiGraph):
         self.add_node(R)
         self.add_edge("root", R)
     
-    def insertSteinerNode(self, *args, steiner_R=None, leaf1_R=None, leaf2_R=None):
-        for i, param in enumerate(steiner_R, leaf1_R, leaf2_R):
-            param = args[i] if param is None else param
-
+    def insertSteinerNode(self, steiner_R=None, leaf1_R=None, leaf2_R=None):
         self.add_node(steiner_R)
         self.add_edge("root", steiner_R)
         for leaf_R in (leaf1_R, leaf2_R):
