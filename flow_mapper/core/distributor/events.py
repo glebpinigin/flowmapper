critical bug fixed in events.py. Collapse arguments calculation now stable

flow_mapper/core/distributor/events.py
@@ -3,7 +3,7 @@ from urllib.parse import _SplitResultBase
 import numpy as np
 from .spiraltree import SpiralTree
 from .wavefronts import W, WData
-from .local_utils import intersect_curves, rl_inverse, intersect_num, rect_logspiral, calcR
+from .local_utils import intersect_curves, rl_inverse, intersect_num, rect_logspiral, calcR, signlr
 from .spirals import NodeRegion
 from .abst import AbstractSearchTree, AbstractBSTData
 
@@ -131,12 +131,14 @@ class TerminalEvent:
                 falseR = NodeRegion(curve1.root, inter_crds, fake_params,alpha=curve1.deg_alpha, volumes=out_volumes)
 
                 # need another point for splines
-                if new_phi < mid_ang:
+                if new_phi != mid_ang:
+                    ntp = signlr(-np.sign(mid_ang-new_phi))
                     new_phi_2 = new_phi + (mid_ang-new_phi)/2
-                    new_r_2 = calcR(new_phi_2, nb.R.params, "left")
-                else:
-                    new_phi_2 = mid_ang + (new_phi-mid_ang)/2
-                    new_r_2 = calcR(new_phi_2, nb.R.params, "right")
+                    new_r_2 = calcR(new_phi_2, nb.R.params, ntp)
+                elif new_phi == mid_ang:
+                    ntp = signlr(-np.sign(self.R.ang-new_phi))
+                    new_phi_2 = nb.R.ang + (new_phi-self.R.ang)/2
+                    new_r_2 = calcR(new_phi_2, nb.R.params, ntp)
                 
                 addtn = rect_logspiral(new_r_2, new_phi_2)
 