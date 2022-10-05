Symbol properties API edited; Node offset calculation fixed (not tested)

flow_mapper/core/distributor/spiraltree.py
@@ -5,7 +5,7 @@ from shapely.geometry import LineString
 
 
 class SpiralTree(nx.DiGraph):
-    
+    symbol_attrs = {"linewidth", "offset", "color"}
 
     def __init__(self, root, bias, vol_attrs=None):
         super().__init__()
@@ -53,6 +53,34 @@ class SpiralTree(nx.DiGraph):
             leaf = tuple(np.array(val.leaf) + bias)
             self.Rs[val] = leaf
         return leaf
+
+    def setSymbolProperty(self, property_name, value, attr_name_array=None):
+        # NB!!! symbol property can be mutable object
+        if property_name not in self.symbol_attrs:
+            raise ValueError(f"""Invalid symbol property name.
+                            Valid property names are {self.symbol_attrs}""")
+        if attr_name_array is None:
+            # setting default value for whole tree
+            setattr(self, '_' + property_name, value)
+        elif all(name in self.vol_attrs for name in attr_name_array):
+            nx.set_edge_attributes(self, value, name=property_name)
+        else:
+            raise ValueError(f"""Unknown attribute in {attr_name_array}.
+                            Valid attributes are {self.vol_attrs}""")
+    
+    def symbolProperty(self, property_name, attr_name_array=None):
+        if property_name not in self.symbol_attrs:
+            raise ValueError(f"""Invalid symbol property name.
+                            Valid property names are {self.symbol_attrs}""")
+        if attr_name_array is None:
+            return getattr(self, '_' + property_name)
+        elif attr_name_array == "all":
+            return self.edges.data(property_name)
+        elif all(name in self.vol_attrs for name in attr_name_array):
+            return self.edges.data(property_name)
+        else:
+            raise ValueError(f"""Unknown attribute in {attr_name_array}.
+                            Valid attributes are {self.vol_attrs}""")
 
 
 def connectionsToWkt(T: SpiralTree):

flow_mapper/core/drawer/pptr.py
@@ -98,10 +98,12 @@ def ptsToSpline(pts, ptnum=20, kind="cubic"):
     return interpolator(alpha)
 
 
-def smoothTr(T: nx.DiGraph, ptnum=21, kind="cubic", inplace = False):
+def smoothTr(T: nx.DiGraph, ptnum=21, kind="cubic", inplace = True, geom_field="raw_geom"):
     gs = list(map(lambda x: LineString(ptsToSpline(
-                    np.array(x[2]["raw_geom"].coords))),
+                    np.array(x[2][geom_field].coords), ptnum, kind)),
                     T.edges.data()))
     vals = dict(zip(T.edges, gs))
-    nx.set_edge_attributes(T, vals, name="spline_geom")
+    if inplace:
+        nx.set_edge_attributes(T, vals, name="spline_geom")
+        return None
     return vals

flow_mapper/core/drawer/thcktr.py
@@ -1,28 +1,33 @@
 import networkx as nx
 from flow_mapper.core.distributor import local_utils as lu
 import numpy as np
+import warnings
 
 # шкалирование
 def linScale(x, frommin, frommax, tomin, tomax):
+    # TODO: add soft zero scaling
     return (tomax-tomin)*(x-frommin)/(frommax-frommin) + tomin
 
 # расчёт ширин в таблицу атрибутов
-def thckTr(T, vol_flds, *scale_args):
+def thckTr(T, vol_flds, *scale_args, inplace=True):
     edge_data_new = {}
     for node1, node2, data in T.edges.data():
-        print(data)
         values = list(map(lambda x: linScale(data[x], *scale_args), vol_flds))
         data_new = {}
-        data_new = {name + "_width": value for name, value in zip(vol_flds, values)}
+        data_new = {name: value for name, value in zip(vol_flds, values)}
         edge_data_new[(node1, node2)] = data_new
-    nx.set_edge_attributes(T, edge_data_new)
-        
+    if inplace:
+        T.setSymbolProperty("linewidth", edge_data_new, vol_flds)
+        return None
+    return edge_data_new
+
+
 # раздвижение узлов (универсальное)
-def moveLastNode(pt, backpt, tp, width):
+def nodeOffset(pt, backpt, tp, width_pt, width_back):
     sign = lu.lrsign(tp)
     _, back_bearing = lu.dst_bearing(pt, backpt)
     move_bearing = back_bearing + sign*np.pi/2
-    dx, dy = lu.rect_logspiral(width/2, move_bearing)
+    dx, dy = lu.rect_logspiral(width_back/2-width_pt/2, move_bearing)
     ptnx = pt[0] + dx
     ptny = pt[1] + dy
     return ptnx, ptny

flow_mapper/core/plotting.py
@@ -58,12 +58,16 @@ def tplot(crds, leaf=None, root=None, tp=None, ax1=None, ax2=None, polar = True,
     return ax1, ax2
 
 
-def drawTree(T, geom_field="raw_geom"):
-    fig = plt.figure(1, (8, 5))
-    ax = fig.add_subplot()
+def drawTree(T, ax=None, geom_field="raw_geom", display_nodes=True, nodes_fmt='go', nodes_kwargs=None):
+    if ax is None:
+        fig = plt.figure(1, (8, 5))
+        ax = fig.add_subplot()
     for node1, node2, geom in T.edges.data(geom_field):
         ax.plot(*np.array(list(geom.coords)).T)
-        ax.plot(*node2, "og")
+    if display_nodes:
+        for node1, node2, geom in T.edges.data(geom_field):
+            nodes_kwargs = {} if nodes_kwargs is None else nodes_kwargs
+            ax.plot(*node2, nodes_fmt, **nodes_kwargs)
     ax.set_aspect(1)
     return ax
 

test.py
@@ -17,44 +17,40 @@ print("Imports succesfull\n")
 r = 1
 errs = 0
 for i in range(r):
+    # preparing sample data
     leaves = getLeaf(8)
-    # vls = [
-    #     ["amount"],
-    #     [[5], [3], [4], [1], [9]]
-    # ]
-    # leaves = list(filter(lambda x: x[0] > 0, leaves))
-    #leaves.append((-25.11407890334805, -153.18910434475615))
-    #leaves = [(-20, 5), (-21, -7)] 
+    vlarray = np.random.rand(len(leaves))*10000%1000
+    vls = [
+        ["amount"],
+        [[i] for i in vlarray]
+    ]
     begin_time = datetime.datetime.now()
-    # try:
-    # T = tbldr.buildTree(leaves=leaves, alpha=35, logshow=0, stop_dst=0, vol_attrs=vls)
-    T = tbldr.buildTree(leaves=leaves, alpha=35, logshow=0, stop_dst=0)
+    # building tree topology
+    T = tbldr.buildTree(leaves=leaves, alpha=35, logshow=0, stop_dst=0, vol_attrs=vls)
+    # building tree geometry
+    pptr.ppTr(T, 4)
+    # smoothing tree geometry
+    pptr.smoothTr(T, 21, inplace=True)
+    # calculating line widths
+    thcktr.thckTr(T, ["amount", "count"], 1, 20, 5, 7)
+
     times = np.append(times, datetime.datetime.now() - begin_time)
-    # except Exception as e:
-    #     errs += 1
-    #     print(f"Error {e} at {i}")
-    # tdraw(list(T.nodes)[1:])
-    # plt.show()
-    # plt.savefig(f"../figs/40/fig{i}.png")
-    # plt.clf()
+
+    # testing tree plotting
+    plotting.drawTree(T, geom_field="raw_geom")
+    plt.clf()
+    plotting.drawTree(T, geom_field="spline_geom")
+    plt.clf()
 
 print(times)
 print(times.mean())
 print(f"{errs} errors / {errs/r*100}%")
 
-pptr.ppTr(T, 4)
-plotting.drawTree(T)
-# pptr.run()
-plt.show()
-
-thcktr.thckTr(T, ["amount", "count"], 1, 20, 5, 7)
-for node1, node2, data in T.edges.data():
-        print(data)
-save = input("Save? y/n ")
-save = True if save == "y" else False
-if save:
-    savename = input("savename? ")
-    lines = momepy.nx_to_gdf(T, points=False)
-    lines['Wkt'] = lines['Wkt'].apply(wkt.loads)
-    lines.set_geometry(col='Wkt', inplace=True)
-    lines.to_file(f"../shape/{savename}.shp")
+# save = input("Save? y/n ")
+# save = True if save == "y" else False
+# if save:
+#     savename = input("savename? ")
+#     lines = momepy.nx_to_gdf(T, points=False)
+#     lines['Wkt'] = lines['Wkt'].apply(wkt.loads)
+#     lines.set_geometry(col='Wkt', inplace=True)
+#     lines.to_file(f"../shape/{savename}.shp")